fb6bb6399da6355d959434eed5ce12d80e5d6534
0x8FAB <88802421+0x8FAB@users.noreply.github.com>
2023-05-10 13:14:36 +0200

    Added default HTML to editable pages

--- server/models/Page.js
+++ server/models/Page.js
@@ -13,16 +13,4 @@ const pageSchema = new mongoose.Schema({
 
 const Page = mongoose.model("Page", pageSchema);
 
-// const aboutPageTest = new Page({
-//   name: "about",
-//   content: "<h1>Hello, About!</h1>",
-// });
-// aboutPageTest.save();
-
-// const aboutPageTest = new Page({
-//   name: "equipment",
-//   content: "<h1>Hello, About!</h1>",
-// });
-// aboutPageTest.save();
-
 exports.Page = Page;
